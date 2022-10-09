08220bc72bebcc53f2743f528c5bea182796929a
Nidhi Kumari <kumarinidhimain@gmail.com>
2022-10-09 07:10:12 +0530

    added postcss.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello My name is Nidhi</h1>
+      <h1 className="text-3xl font-bold underline">Hello My name is Nidhi</h1>
       <h1>Vite + React</h1>
 
       <p>Click on the Vite and React logos to learn more</p>
