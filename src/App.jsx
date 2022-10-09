bac60f352c1afad3419c423d03c94ad5c1f83a55
Nidhi Kumari <kumarinidhimain@gmail.com>
2022-10-09 06:51:00 +0530

    pushed file structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello My name is Nidhi</h1>
+      <h1>Vite + React</h1>
+
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default App;
